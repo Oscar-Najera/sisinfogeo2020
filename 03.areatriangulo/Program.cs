ba1ee5b9c172fd6677d1593b863d97e0e4fa0088
Oscar-Najera <oscarnajera_22@hotmail.com>
2020-01-30 11:57:41 -0600

    La ultima y nos vamos

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double LaBase,altura,area;
+            Console.WriteLine("Programa que calcula el area de un triangulo");
+            Console.WriteLine("Dame la base: ");
+            LaBase=double.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura: ");
+            altura=double.Parse(Console.ReadLine());
+            area=(LaBase*altura)/2;
+            Console.WriteLine($"El area es: {area}");
+
+        }
+    }
+}
