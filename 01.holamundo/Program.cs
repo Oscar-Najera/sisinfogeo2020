eca249b4b11f4b8f10cb2fc39479e961f0a63b96
Oscar-Najera <oscarnajera_22@hotmail.com>
2020-01-30 11:21:10 -0600

    va todo otra vez

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola mundo!");
+	    Console.WriteLine("Bienvenido a programacion en C#");
+        }
+    }
+}
