55d3dcded7af9d370a13a276305305082fbfff9f
Oscar-Najera <oscarnajera_22@hotmail.com>
2020-02-06 15:38:36 -0600

    Resubiendo tarea

--- 06.tablas/Program.cs
+++ 06.tablas/Program.cs
@@ -6,16 +6,36 @@ namespace _06.tablas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Imprimir una tabla de multiplicar específica hasta cierto número");           
-            imprimirTablaMultiplicar(int.Parse(args[0]),int.Parse(args[1]));
+           
+            //    Console.WriteLine("Imprimir una tabla de multiplicar específica hasta cierto número");           
+            //   imprimirTablaMultiplicar(int.Parse(args[0]),int.Parse(args[1]));
+
+          
+                Console.WriteLine("Imprimir las tablas deseadas hasta el número deseado.");
+                imprimirTablaMultiplicarAvanzado(int.Parse(args[0]),int.Parse(args[1]),int.Parse(args[2]));
+            
         }
 
         static void imprimirTablaMultiplicar(int tabla, int limite){
+            limite++;
             for(int i=0;i<limite;i++){
                 int resultado=0;
                 resultado=tabla*i;
                 Console.WriteLine($"{tabla} X {i} = {resultado}");
             }
+        }
+
+        static void imprimirTablaMultiplicarAvanzado(int tablaFinal, int limInf, int limSup){
+            for(int i=0;i<=tablaFinal;i++){
+                for(int j=limInf; j<=limSup;j++){
+                    int resultado=0;
+                    resultado=i*j;
+                    Console.Write($"{i} X {j} = {resultado} \t");
+                };
+                Console.WriteLine();
+                Console.WriteLine("-------------------------------------------");
+            }
+
         }
     }
 }
