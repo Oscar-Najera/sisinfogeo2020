bc09b41d9cd8ac8f5235181c466b8b3e72f298d9
Oscar-Najera <oscarnajera_22@hotmail.com>
2020-02-06 11:32:21 -0600

    Despues de clase

---
+++ 06.tablas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.tablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprimir una tabla de multiplicar específica hasta cierto número");           
+            imprimirTablaMultiplicar(int.Parse(args[0]),int.Parse(args[1]));
+        }
+
+        static void imprimirTablaMultiplicar(int tabla, int limite){
+            for(int i=0;i<limite;i++){
+                int resultado=0;
+                resultado=tabla*i;
+                Console.WriteLine($"{tabla} X {i} = {resultado}");
+            }
+        }
+    }
+}
---
+++ 07.vectorpromedio/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _07.vectorpromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector={10,20,30,40,50,60,70,80,90,100,
+                        10,20,30,40,50,60,70,80,90,100,
+                        10,20,30,40,50,60,70,80,90,100,
+                        10,20,30,40,50,60,70,80,90,100,
+                        10,20,30,40,50,60,70,80,90,100,            
+                        };
+            int suma=0;
+            float promedio =0;
+            Console.WriteLine("Calcular el promedio de 50 valores constantes! \n");
+            Console.WriteLine("Los elementos del arreglo son \n");
+            imprime(vector);     
+            promedio=prom(vector);
+            Console.WriteLine($"\nEl promedio es: {promedio} "); 
+            Console.WriteLine($"La cantidad de numeros mayores al promedio son {mayores(vector,promedio)}");  
+        }
+
+        static int mayores(int[] vector,float p){
+            int contador=0;
+            for(int i=0;i<vector.Length;i++){
+                if(vector[i]>p){
+                    contador++;
+                }
+
+            }
+            return contador;
+        }
+
+        static void imprime(int[] v){
+            for(int i=0; i<v.Length;i++){
+                Console.Write($"{v[i]} ");
+            }
+
+        }
+
+        static float prom (int[] v){
+            int suma =0;
+            for(int i=0; i<v.Length;i++){
+                suma+=v[i];
+            }
+            return suma/v.Length;
+
+        }
+    }
+}
