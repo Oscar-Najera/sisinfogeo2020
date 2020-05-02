cc125f085d9aabc7eb77d1a098b5299d0a33b698
Oscar-Najera <oscarnajera_22@hotmail.com>
2020-05-01 20:13:35 -0500

    Covi practicas 01

---
+++ 25.Interfaz01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _25.Interfaz01
+{
+    class Program
+    {
+
+        public interface IAnimal{
+            string Nombre{get;set;}
+            void Llorar();
+        }
+
+        class Perro : IAnimal{
+            public Perro(string nombre) => Nombre=nombre;
+            public string Nombre {get; set;}
+            public void Llorar() => Console.WriteLine("Wooff Wooff !!");
+        }
+
+        class Gato : IAnimal {
+            public Gato(string nombre) => Nombre=nombre;
+            public string Nombre{get;set;}
+            public void Llorar() => Console.WriteLine("Meeow Meeow !!");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------- Programa de interfaces ----------");
+            Perro perro = new Perro("Sabuesa");
+            Console.WriteLine($"El perro {perro.Nombre} -------- ");
+            perro.Llorar();
+            
+            Gato gato= new Gato("Misifuos");
+            Console.WriteLine($"El gato {gato.Nombre} ----------");
+            gato.Llorar();
+        }
+    }
+}
---
+++ 26.Interfaz02/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _26.Interfaz02
+{
+    public class Organismo
+    {
+        public void Respiracion(){
+            Console.WriteLine("Respiracion");
+        }
+        public void Movimiento(){
+            Console.WriteLine("Movimiento");
+        }
+        public void Crecimiento(){
+            Console.WriteLine("Crecimiento");
+        }
+    }
+    public interface IAnimales{
+        void MultiCelular();
+        void SangreCaliente();
+    }
+    public interface ICanino: IAnimales{
+        void Corre();
+        void CuatroPatas();
+    }
+    public interface IPajaro: IAnimales{
+        void Vuela();
+        void DosPatas();
+    }
+    public class Perro : Organismo, ICanino{
+        public void MultiCelular()=> Console.WriteLine("Multicelular");
+        public void SangreCaliente()=> Console.WriteLine("Sangre caliente");
+        public void Corre() => Console.WriteLine("Corre");
+        public void CuatroPatas() => Console.WriteLine("Cuatro patas");     
+    }
+    public class Perico : Organismo, IPajaro {
+        public void MultiCelular() => Console.WriteLine("Multicelular");
+        public void SangreCaliente() => Console.WriteLine("Sangre caliente");
+        public void Vuela()=> Console.WriteLine("Vuela");
+        public void DosPatas()=>Console.WriteLine("Dos patas");
+    }
+
+    class Program{
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------- Segundo programa de interfaces. -----------");
+            Perro perro =new Perro();
+            Console.WriteLine("\n Caracateristicas del perro: \n");
+            perro.Respiracion();
+            perro.Movimiento();
+            perro.Crecimiento();
+            perro.Corre();
+            perro.CuatroPatas();
+
+            Perico perico=new Perico();
+            Console.WriteLine("\n Caracteristicas del perico: \n");
+            perico.Respiracion();
+            perico.Movimiento();
+            perico.Crecimiento();
+            perico.Vuela();
+            perico.DosPatas();
+        }
+
+    }
+
+}
+
