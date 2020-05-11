111e33bcf2491be4cb232091f0a294889a0f0443
Oscar-Najera <oscarnajera_22@hotmail.com>
2020-05-11 17:04:36 -0500

    Multihilos terminado

---
+++ 27.Threading01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+namespace _27.Threading01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- Programa 27 Hilos -----------");
+            //Nombramos el thread principal
+            Thread.CurrentThread.Name="Principal";
+
+            //Creamos dos thread.
+            Thread t1=new Thread(Imprime);
+            Thread t2=new Thread(Imprime);
+
+            //Damos nombre a los threads que creamos.
+            t1.Name="Thread 01";
+            t2.Name="Thread 02";
+
+            //Invocamos los threads.
+            t1.Start();
+            t2.Start();
+
+            //Invocamos Imprime() desde el thread principal.
+            Imprime();
+
+            Console.WriteLine("Saludos desde Main, se ha terminado todo.");
+        }
+
+        static void Imprime(){
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Imprime . {i} desde {Thread.CurrentThread.Name}.");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ 30.Threading04/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace _30.Threading04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- Programa 30 Multihilos -----------");
+            Console.WriteLine("Hilo principal iniciado");
+            //Crear threads con nombre.
+            Thread t1=new Thread(Metodo01) {Name="Cliente 01"};
+            Thread t2= new Thread(Metodo02) {Name= "Cliente 02"};
+            Thread t3 = new Thread(Metodo03) {Name = "Cliente 03"};
+
+            //Ejecutando métodos.
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.WriteLine("Thread principal terminado.");
+        }
+
+        static void Metodo01(){
+            Console.WriteLine("Método 01 iniciado usando "+Thread.CurrentThread.Name);
+            for(int i=1; i <= 5 ; i++)
+                Console.WriteLine("Método 01 : "+i);
+            Console.WriteLine("Método terminado usando "+Thread.CurrentThread.Name);
+        }
+
+        static void Metodo02(){
+            Console.WriteLine("Método 02 iniciado usando "+Thread.CurrentThread.Name);
+            for(int i=1; i<=5; i++){
+                Console.WriteLine("Método 2 : "+i);
+                if(i==3){
+                    Console.WriteLine("Iniciando operación sobre la base de datos ");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Operación sobre la base de datos terminada");
+                }
+            }
+            Console.WriteLine("Método 2 terminado usando "+Thread.CurrentThread.Name);
+        }
+
+        static void Metodo03(){
+            Console.WriteLine("Método 03 iniciado usando "+Thread.CurrentThread.Name);
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Método 3 : "+ i);
+            }
+            Console.WriteLine("Método 3 terminado usando "+Thread.CurrentThread.Name);
+        }
+
+
+    }
+}
